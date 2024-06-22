e21dfe15f945d75825e0867e7359e777ac7b7524
Denis Semikolenov <Dnis0847@yandex.ru>
2024-06-22 18:46:14 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def print_hi(name):
+def print_hi(name: str) -> None:
+    """
+    This function prints a greeting message with the given name.
+
+    Args:
+        name (str): The name to be included in the greeting.
+
+    Returns:
+        None
+    """
     print(f'Hi, {name}')
 
-
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('World')
